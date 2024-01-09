be22bec5f7fe1b407a19ab25631eadefe42c78e3
Christian <christian.frisancho@utec.edu.pe>
2024-01-09 14:32:40 -0500

    clases

---
+++ single_linked_list.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+};
+
+class List
+{
+public:
+    Node *head;
+    Node *tail;
+};
+int main()
+{
+}
